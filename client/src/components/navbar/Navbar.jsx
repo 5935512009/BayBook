d65a9e1270a4c2490a18422d272bc6786a3e4bac
5935512009 <Pasut2541@hotmail.com>
2024-10-31 13:53:41 +0700

    update:{Navbar UI}

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import icons from '../../assets/icons/BayBook-icons.png'
 export default function Navbar() {
   return (
-    <div>
-      <ul>
-        <li>
-            <Link to='./Home'>Home</Link>
-        </li>
-      </ul>
+    <div className="nav">
+      <div className="nav-left">
+        <img src={icons} alt=" icon not work." />
+        <ul>
+          <li className="lists">
+            <Link to="./Home">Home</Link>
+            <Link to="./About">About</Link>
+            <Link to="./blogs">Blogs</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="nav-right">
+        <button className="log-in">Log in</button>
+        <button className="sign-up">Sign up</button>
+      </div>
     </div>
-  )
+  );
 }
