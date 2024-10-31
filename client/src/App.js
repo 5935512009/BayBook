47f4c918620b3eb4babac8ce90a0fd45a1b751fd
5935512009 <Pasut2541@hotmail.com>
2024-10-31 13:52:49 +0700

    update:{Routes path}

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
+import About from './pages/about/About';
+import Blogs from './pages/blog/Blogs';
 function App() {
   return (
     <div className="App">
@@ -9,7 +11,9 @@ function App() {
         <BrowserRouter>
           <Navbar/>
           <Routes>
-            <Route path='/home' element={<Home/>} />
+            <Route path='/Home' element={<Home/>} />
+            <Route path='/About' element={<About/>} />
+            <Route path='/Blogs' element={<Blogs/>} />
           </Routes>
         </BrowserRouter>
       </div>
