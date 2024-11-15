58b9af1ae4a816d3c4d1bd61b6176c58048fcc1f
5935512009 <Pasut2541@hotmail.com>
2024-11-15 14:37:33 +0700

    update:{hero css }

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './hero.css'
+import hero_books from '../../assets/icons/hero-books.jpg'
 export default function Hero() {
   return (
     <div className='hero'>
-      Hero
+      <div className="hero-left">
+        <div className='hero-left-letter'>
+          Buy many books <br/>
+          for best price
+        </div>
+        <p>Read for life and learn for your.<br/>
+          we provide only the best books rent.
+        </p>
+        <div className='search-bar'>
+          <input type="text" placeholder="Search.."/>
+          <button>Search</button>
+        </div>
+      </div>
+      <div className="hero-right">
+          <img src={hero_books} alt="no images" />
+      </div>
     </div>
   )
 }
