7dceb2b5d73d177157cceb37aa4c9c5786ca3e8f
5935512009 <Pasut2541@hotmail.com>
2024-11-20 17:02:58 +0700

    NEW:{shop page}

---
+++ client/src/pages/shop/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './shop.css'
+import ShopCard from './ShopCard'
+function Shop() {
+  return (
+    <div>
+      <ShopCard/>
+    </div>
+  )
+}
+
+export default Shop
---
+++ client/src/pages/shop/ShopCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShopCard() {
+  return (
+    <div className='shopCard'>
+      shop Card
+    </div>
+  )
+}
+
+export default ShopCard
